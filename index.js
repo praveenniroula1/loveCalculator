f043dcf98292d2181605a83f7185653fbbe3f7a1
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-28 08:14:40 +1100

    making a love calculator

---
+++ index.js
@@ -0,0 +1,7 @@
+const input = document.querySelectorAll(".input");
+console.log(input);
+
+const handleOnClick = (e) => {
+  const data = document.querySelectorAll(".input").value;
+  console.log(data);
+};
