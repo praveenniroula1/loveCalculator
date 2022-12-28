7c1557f28987dc24ffdb5076e07a5856a0ff8326
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-28 11:22:34 +1100

    love calculator functioning somewhat

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const inputFeild = document.querySelectorAll(".input");
+let percent = "";
+const displayLove = document.getElementById("display");
 
-const handleOnChange = () => {
-  data = inputFeild.value;
-  console.log(data);
+const button = document.getElementById("button");
+
+const displayLoveScreen = () => {
+  button.addEventListener("click", () => {
+    const loveData = Math.random() * 100;
+    const percentValue = Math.floor(loveData);
+    const percent = percentValue;
+    window.scrollTo(0, 0);
+    displayLove.innerHTML = "You Guys have compatibility of " + percent + "%";
+  });
 };
+displayLoveScreen();
