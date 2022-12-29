642a9d184a3630bf768a81e17d2ef434c987c38f
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-30 00:33:11 +1100

    making it dynamic

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 let percent = "";
 const displayLove = document.getElementById("display");
-
 const button = document.getElementById("button");
 
-const h2 = document.getElementById("h2");
-const h3 = document.getElementById("h3");
-const dataDisplay = () => {
-  const input1 = document.getElementById("input1");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+let input1Value = "";
+let input2Value = "";
+const handleOnClick = () => {
   input1.addEventListener("change", () => {
-    const inputData = input1.value;
-    h2.innerText = inputData;
-    console.log(h2.innerText);
-    return h2.innerText;
+    input1Value = input1.value;
   });
-};
-dataDisplay();
-console.log(h2.innerText);
-const inputData = () => {
-  const input2 = document.getElementById("input2");
   input2.addEventListener("change", () => {
-    const input2Data = input2.value;
-    h3.innerText = input2Data;
-    console.log(h3.innerText);
+    input2Value = input2.value;
   });
 };
-inputData();
+handleOnClick();
+
 const displayLoveScreen = () => {
   button.addEventListener("click", () => {
     const loveData = Math.random() * 100;
     const percentValue = Math.floor(loveData);
     const percent = percentValue;
     window.scrollTo(0, 0);
-
-    if (percent >= 50) {
+    if (input1Value && input2Value && percent >= 50) {
       displayLove.innerHTML =
-        "Congratuation!!!" +
-        dataDisplay() +
-        inputData() +
-        "have compatibility of " +
+        "Congratulaion!!!" +
+        " " +
+        input1Value +
+        " " +
+        "and" +
+        " " +
+        input2Value +
+        " " +
+        "have compatibility of only " +
         percent +
-        "%";
+        "%.";
     } else {
       displayLove.innerHTML =
-        "Oppssss!!! You Guys have compatibility of only " + percent + "%";
+        "Opps!!!" +
+        " " +
+        input1Value +
+        " " +
+        "and" +
+        " " +
+        input2Value +
+        " " +
+        "have compatibility of only " +
+        percent +
+        "%.";
     }
   });
 };
